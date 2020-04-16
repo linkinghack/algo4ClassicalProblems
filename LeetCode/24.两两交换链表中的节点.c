5116ef67f05781dbda420a9193176cbc36c04db2
liulei <liuleiit01@chinamobile.com>
2020-04-17 07:58:24 +0800

    leetcode.24

---
+++ LeetCode/24.两两交换链表中的节点.c
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=c
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* swapPairs(struct ListNode* head){
+    struct ListNode *prev = NULL, *curr, *next, new_head;
+    new_head.next = head;  // new_head初始不属于链表
+    prev = &new_head;
+    while(prev->next && prev->next-> next)
+    {
+        struct ListNode *a = prev->next, *b = prev->next->next;
+        prev->next = b;
+        a->next = b->next;
+        b->next = a;
+        prev = a;
+    }
+
+    return new_head.next; // new_head无数据,跳过
+}
+
+
+// @lc code=end
+
