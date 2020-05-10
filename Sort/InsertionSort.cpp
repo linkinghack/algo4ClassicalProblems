e254fd1ee1ca81e143d794f2c69b0aa65fa22d80
linkinghack <linking@linkinghack.com>
2020-05-10 16:50:24 +0800

    Update insertion sort

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -6,29 +6,22 @@
 using namespace std;
 
 void StraightInsertionSort(KeyType L[],int length){
-    int i = 0, j = 0;
-    int count = 0;
-    for(i = 2; i<=length; i++){
-        printf("i=%d, L[0]=%d, L[i]=%d ",i,L[0],L[i]);
-        L[0] = L[i];  // 设置哨兵
-        for(j = i-1; L[0]<L[j];j--){  // i-1 是有序部分中最后一个元素
-            L[j+1] = L[j]; // 后移
-            count++;
-        } // j 最终为正确位置
-        printf("j=%d ",j);
-        L[j+1] = L[0]; // 放置
-        printf("L[j]=%d \n",L[j]);
+    for(int i = 1; i < length; i++) {
+        int j = i;
+        KeyType temp = L[i];
+        while (j > 0 && temp < L[j - 1]) {
+            L[j] = L[j-1];
+            j--; // 刚腾出的位置
+        }
+        L[j] = temp;
     }
-    cout<<"Compared "<<count<<" times."<<endl;
 }
 
 int main(){
-    int length = 20;
-    KeyType l[] = {0,2,3,6,1,8,4,56,12,9,5,12,6,7,123,34,213,8,90,62,71};
-    KeyType ll[] = {0,5,4,3,2,1};
+    KeyType ll[] = {5,4,3,2,1};
     StraightInsertionSort(ll,5);
 
-    for(int i=1; i<6; i++) {
+    for(int i=0; i<5; i++) {
         cout<<ll[i]<<" ";
     }
     cout<<endl;
