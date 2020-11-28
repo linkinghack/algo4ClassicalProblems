aec7b7bbad8a81c6893f66200257168e77a566f4
linkinghack <linkinghack@outlook.com>
2020-11-28 21:16:55 +0800

    Leetcode.198:update best solution

--- LeetCode/198.打家劫舍.go
+++ LeetCode/198.打家劫舍.go
@@ -10,7 +10,7 @@ import "math"
  * dp[i][0,1]
  *  dim-0: 偷盗的 max value
  *  dim-1: 偷还是不偷
- * 状态转移：
+ * 状态转移：每层状态都要计算偷和不偷两种情况的结果
  *   a[i][0] = max(a[i-1][0], a[i-1][1])
  *   a[i][1] = a[i-1][0] + nums[i]
  */
@@ -34,6 +34,27 @@ func rob(nums []int) int {
 
 	n := len(nums)
 	return int(math.Max(float64(dp[n-1][0]), float64(dp[n-1][1])))
+}
+
+/**
+ * 方法2 不使用而为数组来记录状态
+ * a[i]  : 0..i 能偷到的最大值, 第i个房子可偷可不偷
+ * DP方程
+ * a[i] = max( a[i-1] + 0,  nums[i] + a[i-2], nums[i] + a[i-3]... )
+ *            决定偷i-1  ,  i-1不偷,状态从i-2转换过来, ...
+ *  nums[i] + a[i-3]... 的状态已经包含在a[i-2]中了, 所以：
+ * a[i] = max( a[i-1] , nums[i] + a[i-2])
+ *
+ * 又因为，每次只需要记录两个状态, a[i-1] , a[i-2]
+ * 所有不需要一个dp数组，只需要两个变量
+ */
+func robMethod2(nums []int) int {
+	prev, now := 0, 0
+	for _, v := range nums {
+		// prev: a[i-2], now: a[i-1]
+		prev, now = now, math.Max(float64(prev+v), now)
+	}
+	return now
 }
 
 // @lc code=end
